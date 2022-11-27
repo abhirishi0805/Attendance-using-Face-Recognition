dac726e02b23fb951541151f0012c7a30592e029
ROHIT AGRAWAL <64243388+rohit1508-gif@users.noreply.github.com>
2022-11-27 16:16:55 +0530

    Date and student_list added

--- app/src/main/java/com/example/attendanceusingfacerecognition/MainActivity.java
+++ app/src/main/java/com/example/attendanceusingfacerecognition/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.attendanceusingfacerecognition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
